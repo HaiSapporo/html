030920b1826fc54bd031d560740ad45d94512bb6
FreeProgrammingCoupon <sownhari@gmail.com>
2022-08-04 23:45:05 +0900

    Update java.java

--- html/java.java
+++ html/java.java
@@ -1,1 +1,9 @@
+
+https://github.com/Corefinder89/SampleJavaCodes/blob/master/src/Dummy1.java
+
 https://www.tutorialspoint.com/how-to-insert-store-json-array-into-a-database-using-jdbc
+
+https://qiita.com/t1k2a/items/733452f191f4bd14d226
+
+
+https://stackoverflow.com/questions/62268311/java-get-json-data-from-url
